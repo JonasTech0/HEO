4255d80f6a6e6d55a3cde830ec80ef531ac509a3
JonasTech0 <Jonastech007@gmail.com>
2024-03-17 01:43:00 +0800

    Site updated: 2024-03-17 01:43:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/03/26/hello-world/","2024/02/15/isTime/"];function toRandomPost(){
+var posts=["2023/03/26/hello-world/","2024/03/17/isTime/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
