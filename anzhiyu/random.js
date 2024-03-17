677bda4131f4fa412b173de0c503520d291d772e
JonasTech0 <Jonastech007@gmail.com>
2024-03-18 03:54:16 +0800

    Site updated: 2024-03-18 03:54:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/03/26/hello-world/","2024/03/17/isTime/"];function toRandomPost(){
+var posts=["2023/03/26/hello-world/","2024/03/17/240317-超低价升级开通GPT4、Claude、MJ等服务!/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
