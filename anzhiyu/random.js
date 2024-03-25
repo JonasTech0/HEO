a7e71beddc5cf229751e1c43b605d4812c606ad1
JonasTech0 <Jonastech007@gmail.com>
2024-03-25 17:19:50 +0800

    Site updated: 2024-03-25 17:19:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/17/GPTPLUS/GPT4/","2023/03/26/TBug/dingtalk/","2024/02/05/bing/Vercel/","2023/03/26/bing/newbing/","2021/12/23/cut/zyhz/"];function toRandomPost(){
+var posts=["2024/03/17/GPTPLUS/GPT4/","2024/02/05/bing/Vercel/","2023/03/26/bing/newbing/","2021/12/23/cut/zyhz/","2023/03/26/TBug/dingtalk/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
