70017e6b047af18fc4ea95a26611f2fa8005167b
JonasTech0 <Jonastech007@gmail.com>
2024-03-26 17:18:10 +0800

    Site updated: 2024-03-26 17:18:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/03/26/TBug/dingtalk/","2021/12/23/montage/zyhz/","2024/01/13/Github/Open-Interpreter/","2024/03/17/AI era/GPTPLUS/GPT4/","2024/02/27/AI era/deploy/One-Code/","2024/03/01/AI era/deploy/Vercel/","2024/02/05/AI era/bing/Vercel/","2024/02/06/AI era/bing/newbing/"];function toRandomPost(){
+var posts=["2024/01/13/Github/Open-Interpreter/","2023/03/26/TBug/dingtalk/","2021/12/23/montage/zyhz/","2024/03/17/AI era/GPTPLUS/GPT4/","2024/02/05/AI era/bing/Vercel/","2024/02/06/AI era/bing/newbing/","2024/02/27/AI era/deploy/One-Code/","2023/10/27/AI era/amaze/Heygen/","2024/03/01/AI era/deploy/Vercel/","2023/10/26/AI era/mirror/LobeChat/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
